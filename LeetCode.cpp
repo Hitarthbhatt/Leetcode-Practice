a967c7f031aae4226a9d93a0ead14f06b3177233
Hitarth Bhatt <hitarthbhatt12@gmail.com>
2024-10-02 07:22:23 +0530

    Updating folder structure

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -6,22 +6,28 @@
 
 using namespace std;
 
+bool isVowel(char& ch) {
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'y') {
+        return true;
+    }
+    return false;
+}
 
 int main() {
 
-    vector<vector<int> > ans(5);
-    int i = 0;
+    string s;
+    string result;
+    getline(cin, s);
 
-    for (int i = 0; i < ans.size(); i++) {
-        ans[i].resize(2);
-        cout << "[ ";
-        for (int j = 0; j < 2; j++) {
-            cout << ans[i][j];
+    for (int i = 0; i < s.length(); i++) {
+        char ch = tolower(s[i]);
+        if (!isVowel(ch)) {
+            result.push_back('.');
+            result.push_back(ch);
         }
-        cout << " ], ";
     }
 
-    cout << endl;
+    cout << result;
 
     return 0;
 }
